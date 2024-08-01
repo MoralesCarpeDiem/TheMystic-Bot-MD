2e857b2aaade30cc843e0c0ba25856197ab61881
MoralesCarpeDiem <hectorapolomoralesdavila@gmail.com>
2024-08-01 02:03:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
-global.confirmCode = ""
+global.botnumber = "528154354432"
+global.confirmCode = "528136964109"
 
 global.owner = [
   ['5219992095479', '👑 Mystic - Creador 👑', true],
